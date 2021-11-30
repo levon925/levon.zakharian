0466073562281d5972651abded849dc07f372223
levon925 <76520734+levon925@users.noreply.github.com>
2021-12-01 01:16:20 +0400

    Add files via upload

---
+++ js/scrolling-up.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    const scrollUp = document.getElementById("scrolling-up")
+    console.log(scrollUp);
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollUp.style.display = "block";
+    } else {
+        scrollUp.style.display = "none";
+    }
+}
