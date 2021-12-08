03fbcbdeec1022b4d905d91fb5e83db43e4b78e7
levon925 <76520734+levon925@users.noreply.github.com>
2021-12-09 00:03:45 +0400

    Delete js directory

--- js/more-content.js
+++
@@ -1,9 +0,0 @@
-const showMoreBtn = document.querySelector('.show-more-button');
-const showMoreContent = document.getElementById('block1');
-const showMoreDiv = document.getElementById('show-more')
-
-showMoreBtn.onclick = function(){
-    showMoreContent.classList.remove('display-none')
-    showMoreDiv.classList.add('display-none')
-    
-}
